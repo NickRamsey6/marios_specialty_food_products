92691c6f1a03b7143dfae9517b008b41906a81c6
Nick Ramsey <nramseysc@gmail.com>
2019-08-09 16:20:34 -0700

    controller fixes for Reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -54,6 +54,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name)
+    params.require(:product).permit(:name, :price)
   end
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,32 @@
+class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
+  end
+
+  def create
+  @product = Product.find(params[:product_id])
+  @review = @product.reviews.new(review_params)
+  if @review.save
+    redirect_to product_path(@product)
+  else
+    render :new
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
+  end
+end
