b8b762c59bb3d9cb1cefcea4b05c64085ffb24a8
Nick Ramsey <nramseysc@gmail.com>
2019-08-09 16:28:33 -0700

    review fully CRUD functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,12 +6,13 @@ class ReviewsController < ApplicationController
   end
 
   def create
-  @product = Product.find(params[:product_id])
-  @review = @product.reviews.new(review_params)
-  if @review.save
-    redirect_to product_path(@product)
-  else
-    render :new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
   end
 
   def show
@@ -23,6 +24,22 @@ class ReviewsController < ApplicationController
   def edit
     @product = Product.find(params[:product_id])
     @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    review.destroy
+    redirect_to product_path(@review.product)
   end
 
   private
