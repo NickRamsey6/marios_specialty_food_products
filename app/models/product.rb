5cbb2b4678ee7208de2a6ab978240618a353ad49
Nick Ramsey <nramseysc@gmail.com>
2019-08-09 23:15:14 -0700

    titleize callback

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :origin, presence: true
+  before_save(:titleize_product)
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,4 +7,9 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :price }
   it { should validate_presence_of :origin }
+  #titleize_product spec
+  it("titleizes the name of a product") do
+    product = Product.create({name: "skyline chili", price: 5, origin: "USA"})
+    expect(product.name()).to(eq("Skyline Chili"))
+  end
 end
