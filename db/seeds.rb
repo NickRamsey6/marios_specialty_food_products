9bbdd9a1e3b27caad8ed753b5d14d43ac278eb33
Nick Ramsey <nramseysc@gmail.com>
2019-08-09 23:25:39 -0700

    product seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food, price: Faker::Number.number(digits: 5), origin: Faker::Nation)
+end
+p "Created #{Product.count} products"
