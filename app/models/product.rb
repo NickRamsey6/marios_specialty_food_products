8453cd05ac30771d518e44ed593606c9436bd0b5
Nick Ramsey <nramseysc@gmail.com>
2019-08-16 12:08:56 -0700

    pre lunch commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  # validates :name, presence: true
-  # validates :price, presence: true
-  # validates :origin, presence: true
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :origin, presence: true
   # before_save(:titleize_product)
   #
   # private
